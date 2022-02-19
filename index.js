d0f8ecd522d952b151040e54065ea751fa0cae27
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-02-19 15:28:19 -0800

    commit tree 4b69f0370c1720b6b6e5441cbdf2c626183cec9b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let eventName = "surprise";
+const recipients = ["Guadalupe","Ollie", "Aki"];
+let message = [];
+
+function writeCards(recipients, eventName) {
+    for (let i = 0; i < recipients.length; i++) {
+       message.push(`Thank you, ${recipients[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return message;
+}
+
+function countDown(startNumber) {
+    let i = startNumber
+    while (i >= 0) {
+        console.log(i--);
+    }
+
+}
